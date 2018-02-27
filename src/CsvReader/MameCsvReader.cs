cdc8bbf6bb8536243d2bfe5ffbcca978a30bcb5c
Thomas Broust <thomas.broust@gmail.com>
2018-02-27 22:55:01 +0100

    Starts library to read and process csv files

---
+++ src/CsvReader/MameCsvReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using CsvHelper;
+
+namespace CsvReader
+{
+    public class MameCsvReader : IDisposable
+    {
+        private readonly StreamReader fileReader;
+        private readonly CsvHelper.CsvReader csvReader;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:MameCsvReader.MameCsvReader"/> class.
+        /// </summary>
+        /// <param name="filePath">The path to the file to read.</param>
+        public MameCsvReader(string filePath)
+        {
+            this.fileReader = File.OpenText(filePath);
+            this.csvReader = new CsvHelper.CsvReader(this.fileReader);
+        }
+
+        /// <summary>
+        /// Releases all resource used by the <see cref="T:MameCsvReader.Reader"/> object.
+        /// </summary>
+        /// <remarks>Call <see cref="Dispose"/> when you are finished using the <see cref="T:MameCsvReader.Reader"/>. The
+        /// <see cref="Dispose"/> method leaves the <see cref="T:MameCsvReader.Reader"/> in an unusable state. After
+        /// calling <see cref="Dispose"/>, you must release all references to the <see cref="T:MameCsvReader.Reader"/>
+        /// so the garbage collector can reclaim the memory that the <see cref="T:MameCsvReader.Reader"/> was occupying.</remarks>
+        public void Dispose()
+        {
+            fileReader?.Dispose();
+            csvReader?.Dispose();
+        }
+    }
+}
