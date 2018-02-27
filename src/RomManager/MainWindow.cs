0a114dde0675c8f80270668bb985a40cbc9cebaa
Thomas Broust <thomas.broust@gmail.com>
2018-02-27 23:57:51 +0100

    Adds a GUI app

---
+++ src/RomManager/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        // disable secondary file picker
+        //this.filechooserSecondary.Sensitive = false;
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void ButtonProcess_Clicked(object sender, EventArgs e)
+    {
+        using (var mainReader = new CsvReader.MameCsvReader(this.filechooserMain.Filename)) {
+            
+        }
+    }
+}
