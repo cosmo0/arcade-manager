79ba7987b3b8039ed428db81ee46b525ac2022ef
Thomas Broust <thomas.broust@gmail.com>
2018-03-03 18:45:07 +0100

    Better UI (why am I doing this while nothing is working ?)

--- src/csv.js
+++ src/csv.js
@@ -1,10 +1,24 @@
 const fs = require('fs');
 const path = require('path');
-const csv = require('fast-csv');
+const csvparse = require('csv-parse/lib/sync');
 
 module.exports = {
+    /**
+     * Combines roms from the main and secondary CSV files,
+     * and saves the result in the target file.
+     * 
+     * @param {string} main The path to the main CSV file
+     * @param {string} secondary The path to the secondary CSV file
+     * @param {string} target The path to the combined file to save
+     */
     add: function add (main, secondary, target) {
-
+        let merge = [];
+        let mainCsv = csvparse(main, {
+            auto_parse: false,
+            auto_parse_date: false,
+            delimiter: ";"
+        });
+        console.log(mainCsv);
     },
 
     remove: function remove (main, secondary, target) {
