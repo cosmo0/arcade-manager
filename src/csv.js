b1fb707e2df2ac3f5d61746ce6f28efd0f8ef221
Thomas Broust <thomas.broust@gmail.com>
2018-03-03 11:42:33 +0100

    Adds empty methods for csv and rom management

---
+++ src/csv.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
+
+module.exports = {
+    add: function add (main, secondary, target) {
+
+    },
+
+    remove: function remove (main, secondary, target) {
+
+    }
+};
---
+++ src/roms.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    add: function add (file, romset, selection) {
+
+    },
+
+    remove: function remove (file, selection) {
+        
+    }
+};
