baadb5d4617bd0d8cf100bedf0e6da039c27d65b
Thomas Broust <thomas.broust@gmail.com>
2021-03-16 22:32:54 +0100

    Copies help pages

---
+++ ArcadeManager/Controllers/HelpController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArcadeManager.Controllers
+{
+    public class HelpController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Basics() => View();
+
+        public IActionResult CustomCsv() => View();
+
+        public IActionResult DatFiles() => View();
+
+        public IActionResult Emulators() => View();
+
+        public IActionResult Install() => View();
+
+        public IActionResult KnownSystems() => View();
+
+        public IActionResult Romsets() => View();
+
+        public IActionResult Shares() => View();
+
+        public IActionResult Tips() => View();
+
+        public IActionResult What() => View();
+    }
+}
