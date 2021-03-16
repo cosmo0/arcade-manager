d19c175bcdc661799768105f6ccaa72fd5053457
Thomas Broust <thomas.broust@gmail.com>
2021-03-17 00:21:26 +0100

    Handles external navigation

--- ArcadeManager/wwwroot/js/site.js
+++ ArcadeManager/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(() => {
+    const { ipcRenderer } = require("electron");
+
+    // new pages navigation
+    $('a.blank').on('click', (e) => {
+        ipcRenderer.send('open-blank', $(e.currentTarget).attr('href'));
+        e.preventDefault();
+    });
+});
