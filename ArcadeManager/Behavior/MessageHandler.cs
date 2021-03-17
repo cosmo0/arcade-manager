8422786530de4cfc105748c7b2353e62025cb726
Thomas Broust <thomas.broust@gmail.com>
2021-03-18 00:35:58 +0100

    Binds file/folder browsing

--- ArcadeManager/Behavior/MessageHandler.cs
+++ ArcadeManager/Behavior/MessageHandler.cs
@@ -1,5 +1,7 @@
 ﻿using ElectronNET.API;
+using ElectronNET.API.Entities;
 using System;
+using System.Threading.Tasks;
 
 namespace ArcadeManager.Behavior {
 
@@ -15,24 +17,87 @@ namespace ArcadeManager.Behavior {
 			if (HybridSupport.IsElectronActive) {
 				Electron.IpcMain.On("open-blank", OpenNewWindow);
 
-				Electron.IpcMain.On("get-os", (args) => {
-					Electron.IpcMain.Send(window, "get-os", Settings.Os);
-				});
-
+				// Get/Set OS
+				Electron.IpcMain.On("get-os", (args) => { GetOs(window); });
 				Electron.IpcMain.On("change-os", ChangeOs);
+
+				// Browse events
+				Electron.IpcMain.On("select-directory", async (args) => { await BrowseFolder(args, window); });
+				Electron.IpcMain.On("new-file", async (args) => { await NewFile(args, window); });
+				Electron.IpcMain.On("select-file", async (args) => { await SelectFile(args, window); });
 			}
+		}
+
+		/// <summary>
+        /// Browse for a folder to select
+        /// </summary>
+        /// <param name="currentPath">The default path</param>
+        /// <param name="window">The window reference</param>
+		private static async Task BrowseFolder(object currentPath, BrowserWindow window)
+        {
+			var options = new OpenDialogOptions
+			{
+				Properties = new OpenDialogProperty[] { OpenDialogProperty.openDirectory },
+				DefaultPath = currentPath as string ?? Environment.GetFolderPath(Environment.SpecialFolder.Personal)
+			};
+
+			string[] files = await Electron.Dialog.ShowOpenDialogAsync(window, options);
+			Electron.IpcMain.Send(window, "select-directory-reply", files);
+		}
+
+		/// <summary>
+        /// Create a new file 
+        /// </summary>
+        /// <param name="path">The default path</param>
+        /// <param name="window">The window reference</param>
+		private static async Task NewFile(object path, BrowserWindow window)
+        {
+			var options = new SaveDialogOptions
+			{
+				DefaultPath = path as string ?? Environment.GetFolderPath(Environment.SpecialFolder.Personal)
+			};
+
+			string file = await Electron.Dialog.ShowSaveDialogAsync(window, options);
+			Electron.IpcMain.Send(window, "new-file-reply", file);
+		}
+
+		/// <summary>
+        /// Selects a file
+        /// </summary>
+        /// <param name="path">The default path</param>
+        /// <param name="window">The window reference</param>
+		private static async Task SelectFile(object path, BrowserWindow window)
+        {
+			var options = new OpenDialogOptions
+			{
+				Properties = new OpenDialogProperty[] { OpenDialogProperty.openFile },
+				DefaultPath = path as string ?? Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+				Filters = new FileFilter[] { new FileFilter { Extensions = new string[] { ".csv" } } }
+			};
+
+			string[] files = await Electron.Dialog.ShowOpenDialogAsync(window, options);
+			Electron.IpcMain.Send(window, "select-file-reply", files);
 		}
 
 		/// <summary>
 		/// Changes the selected OS
 		/// </summary>
-		/// <param name="obj"></param>
+		/// <param name="obj">The value</param>
 		private static void ChangeOs(object obj) {
 			if (obj != null) {
 				// save the OS in settings
 				Settings.Os = obj.ToString();
 			}
 		}
+
+		/// <summary>
+        /// Gets the selected OS
+        /// </summary>
+        /// <param name="window">The window reference</param>
+		private static void GetOs(BrowserWindow window)
+        {
+			Electron.IpcMain.Send(window, "get-os", Settings.Os);
+        }
 
 		/// <summary>
 		/// Opens a new browser window to the specified URL
--- ArcadeManager/wwwroot/js/site.js
+++ ArcadeManager/wwwroot/js/site.js
@@ -6,13 +6,44 @@ $(() => {
         ipcRenderer.send('open-blank', $(e.currentTarget).attr('href'));
         e.preventDefault();
     });
+
+    // bind browse buttons
+    $('.browse').on('click', (e) => {
+        const btn = $(e.target);
+        const input = $('#' + btn.data('input'));
+        const existingPath = input.val();
+
+        if (btn.hasClass('folder')) {
+            // select a folder
+            getFolder(existingPath, (folder) => {
+                if (folder && folder.length !== 0) { input.val(folder); }
+            });
+        }
+        else if (btn.hasClass('new')) {
+            // create a new file
+            newFile(existingPath, (filename) => {
+                if (typeof filename !== 'undefined' && filename !== '') {
+                    if (!filename.endsWith('.csv')) { filename += '.csv'; }
+                    input.val(filename);
+                }
+            });
+        }
+        else {
+            // select a file
+            selectFile(existingPath, (file) => {
+                if (file && file.length !== 0) { input.val(file); }
+            });
+        }
+    });
 });
 
 /**
  * Gets the selected OS (Recalbox/Retropie)
+ *
+ * @param {Function} cb the callback
  */
 function getOs(cb) {
-    ipcRenderer.on('get-os', (event, os) => {
+    ipcRenderer.once('get-os', (event, os) => {
         console.log(`OS: ${os}`);
         if (cb && typeof cb === 'function') {
             cb(os);
@@ -20,4 +51,46 @@ function getOs(cb) {
     });
 
     ipcRenderer.send('get-os');
+}
+
+/**
+ * Gets a folder path
+ * 
+ * @param {String} current the current path
+ * @param {Function} cb the callback
+ */
+function getFolder(current, cb) {
+    ipcRenderer.once("select-directory-reply", (sender, path) => {
+        cb(path);
+    });
+
+    ipcRenderer.send("select-directory", current);
+}
+
+/**
+ * Create a file
+ * 
+ * @param {String} current the current path
+ * @param {Function} cb the callback
+ */
+function newFile(current, cb) {
+    ipcRenderer.once("new-file-reply", (sender, path) => {
+        cb(path);
+    });
+
+    ipcRenderer.send("new-file", current);
+}
+
+/**
+ * Selects a file
+ * 
+ * @param {String} current the current path
+ * @param {Function} cb the callback
+ */
+function selectFile(current, cb) {
+    ipcRenderer.once("select-file-reply", (sender, path) => {
+        cb(path);
+    });
+
+    ipcRenderer.send("select-file", current);
 }
