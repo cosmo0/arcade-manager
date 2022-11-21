579c51af574d77855f96615121fc9bddbbcb1296
cosmo0 <thomas.broust@gmail.com>
2022-11-22 00:11:38 +0100

    Start of wizard

---
+++ ArcadeManager/Controllers/WizardController.cs
@@ -0,0 +1,47 @@
+﻿using ArcadeManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ArcadeManager.Controllers;
+
+/// <summary>
+/// Controller for the wizard pages
+/// </summary>
+public class WizardController : BaseController {
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WizardController"/> class.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    public WizardController(ILogger<WizardController> logger) : base(logger) {
+    }
+
+    /// <summary>
+    /// Index view: selection of actions
+    /// </summary>
+    /// <returns>The view</returns>
+    public IActionResult Index() => View();
+
+    /// <summary>
+    /// Postback to index - 
+    /// </summary>
+    /// <param name="roms">The roms.</param>
+    /// <param name="overlays">The overlays.</param>
+    /// <returns>Redirects to an action</returns>
+    [HttpPost]
+    public IActionResult Index(string roms, string overlays) {
+        var model = new Wizard {
+            DoRoms = !string.IsNullOrEmpty(roms),
+            DoOverlays = !string.IsNullOrEmpty(overlays)
+        };
+
+        if (model.DoRoms) {
+            return RedirectToAction("Roms", model);
+        } else {
+            return RedirectToAction("Overlays", model);
+        }
+    }
+
+    public IActionResult Roms(Wizard model) => View(model);
+}
---
+++ ArcadeManager/Models/Wizard.cs
@@ -0,0 +1,17 @@
+﻿namespace ArcadeManager.Models;
+
+/// <summary>
+/// Model for the wizard
+/// </summary>
+public class Wizard {
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to do the overlays install.
+    /// </summary>
+    public bool DoOverlays { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to do the roms selection.
+    /// </summary>
+    public bool DoRoms { get; set; }
+}
