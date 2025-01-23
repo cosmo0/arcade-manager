4803b0895c1700721b0a4fb82471ae8f3cd8e28a
cosmo0 <thomas.broust@gmail.com>
2025-01-23 23:35:00 +0100

    Fixes #74 - some newer MAME romsets have split some data into additional files

--- src/ArcadeManager/Services/Roms.cs
+++ src/ArcadeManager/Services/Roms.cs
@@ -11,9 +11,11 @@ namespace ArcadeManager.Services;
 /// <summary>
 /// Roms management
 /// </summary>
-public class Roms : IRoms {
+public class Roms : IRoms
+{
     private readonly List<string> bioslist;
     private readonly BiosMatchList biosmatch = new();
+    private readonly DeviceMatchList devicematch = new();
     private readonly ICsv csvService;
     private readonly IFileSystem fs;
 
@@ -22,14 +24,23 @@ public class Roms : IRoms {
     /// </summary>
     /// <param name="csvService">The CSV service.</param>
     /// <param name="fs">The filesystem infrastructure.</param>
-    public Roms(ICsv csvService, IFileSystem fs) {
+    public Roms(ICsv csvService, IFileSystem fs)
+    {
         this.csvService = csvService;
         this.fs = fs;
 
-        this.bioslist = fs.ReadAllLines(fs.GetDataPath("bioslist.txt")).ToList();
-        this.biosmatch.AddRange(fs.ReadAllLines(fs.GetDataPath("biosmatch.csv"))
-            .Select(l => l.Split(";".ToCharArray()))
-            .Select(l => new BiosMatch(l[0], l[1], l[2])));
+        this.bioslist = [.. fs.ReadAllLines(fs.GetDataPath("bioslist.txt"))];
+        this.biosmatch.AddRange(
+            fs.ReadAllLines(fs.GetDataPath("biosmatch.csv"))
+                .Select(l => l.Split(";".ToCharArray()))
+                .Select(l => new BiosMatch(l[0], l[1], l[2]))
+        );
+
+        this.devicematch.AddRange(
+            fs.ReadAllLines(fs.GetDataPath("devices.csv"))
+                .Select(l => l.Split(";".ToCharArray()))
+                .Select(l => new DeviceMatch(l[0], [.. l.Skip(1)]))
+        );
     }
 
     /// <summary>
@@ -39,10 +50,12 @@ public class Roms : IRoms {
     /// <param name="messageHandler">The message handler.</param>
     /// <exception cref="FileNotFoundException">Unable to find main CSV file</exception>
     /// <exception cref="DirectoryNotFoundException">Unable to find romset folder {args.romset}</exception>
-    public async Task Add(Actions.RomsAction args, IMessageHandler messageHandler) {
+    public async Task Add(Actions.RomsAction args, IMessageHandler messageHandler)
+    {
         messageHandler.Init("Copying roms");
 
-        try {
+        try
+        {
             // check files and folders
             if (!fs.FileExists(args.main)) { throw new PathNotFoundException($"Unable to find main CSV file {args.main}"); }
             if (!fs.DirectoryExists(args.romset)) { throw new PathNotFoundException($"Unable to find romset folder {args.romset}"); }
@@ -56,7 +69,8 @@ public class Roms : IRoms {
 
             messageHandler.Done($"Copied {copied} file(s)", args.selection);
         }
-        catch (Exception ex) {
+        catch (Exception ex)
+        {
             messageHandler.Error(ex);
         }
     }
@@ -66,7 +80,8 @@ public class Roms : IRoms {
     /// </summary>
     /// <param name="args">The arguments</param>
     /// <param name="messageHandler">The message handler.</param>
-    public async Task AddFromWizard(RomsAction args, IMessageHandler messageHandler) {
+    public async Task AddFromWizard(RomsAction args, IMessageHandler messageHandler)
+    {
         // the csv is just a list name, we need to match it to the selected path
         string[] emulatorFiles = args.main.Split("/", StringSplitOptions.RemoveEmptyEntries);
         string emulator = emulatorFiles[0];
@@ -74,14 +89,16 @@ public class Roms : IRoms {
 
         messageHandler.Init("Copying roms");
 
-        try {
+        try
+        {
             // check files and folders
             if (!fs.DirectoryExists(args.romset)) { throw new PathNotFoundException($"Unable to find romset folder {args.romset}"); }
             if (!fs.DirectoryExists(args.selection)) { fs.CreateDirectory(args.selection); }
 
             // read CSV files
             Models.CsvGamesList content = new();
-            foreach (var file in files) {
+            foreach (var file in files)
+            {
                 string filepath = fs.GetDataPath("csv", emulator, $"{file}.csv");
                 content.AddRange(await csvService.ReadFile(filepath, false));
             }
@@ -93,7 +110,8 @@ public class Roms : IRoms {
 
             messageHandler.Done($"Copied {copied} file(s)", args.selection);
         }
-        catch (Exception ex) {
+        catch (Exception ex)
+        {
             messageHandler.Error(ex);
         }
     }
@@ -106,7 +124,8 @@ public class Roms : IRoms {
     /// <returns>The missing files list</returns>
     public async Task<string[]> Check(RomsAction args, IMessageHandler messageHandler)
     {
-        try {
+        try
+        {
             // check files and folders
             if (!fs.FileExists(args.main)) { throw new PathNotFoundException($"Unable to find CSV file {args.main}"); }
             if (!fs.DirectoryExists(args.romset)) { throw new PathNotFoundException($"Unable to find folder {args.romset}"); }
@@ -116,16 +135,19 @@ public class Roms : IRoms {
 
             // check that all files in the CSV are listed in the folder
             var result = new List<string>();
-            foreach (var game in content.Games) {
+            foreach (var game in content.Games)
+            {
                 var gamePath = fs.PathJoin(args.romset, $"{game.Name}.zip");
-                if (!fs.FileExists(gamePath)) {
+                if (!fs.FileExists(gamePath))
+                {
                     result.Add(game.Name);
                 }
             }
 
             return [.. result];
         }
-        catch (Exception ex) {
+        catch (Exception ex)
+        {
             messageHandler.Error(ex);
         }
 
@@ -139,10 +161,12 @@ public class Roms : IRoms {
     /// <param name="messageHandler">The message handler.</param>
     /// <exception cref="FileNotFoundException">Unable to find main CSV file</exception>
     /// <exception cref="DirectoryNotFoundException">Unable to find selection folder {args.selection}</exception>
-    public async Task Delete(Actions.RomsAction args, IMessageHandler messageHandler) {
+    public async Task Delete(Actions.RomsAction args, IMessageHandler messageHandler)
+    {
         messageHandler.Init("Deleting roms");
 
-        try {
+        try
+        {
             // check files and folders
             if (!fs.FileExists(args.main)) { throw new PathNotFoundException($"Unable to find main CSV file {args.main}"); }
             if (!fs.DirectoryExists(args.selection)) { throw new PathNotFoundException($"Unable to find selection folder {args.selection}"); }
@@ -154,7 +178,8 @@ public class Roms : IRoms {
             var i = 0;
             var deleted = 0;
 
-            foreach (var game in content.Games.Select(g => g.Name)) {
+            foreach (var game in content.Games.Select(g => g.Name))
+            {
                 if (messageHandler.MustCancel) { break; }
                 i++;
 
@@ -165,13 +190,15 @@ public class Roms : IRoms {
                 messageHandler.Progress(game, total, i);
 
                 // check that source rom exists
-                if (!fs.FileExists(filePath)) {
+                if (!fs.FileExists(filePath))
+                {
                     zip = $"{game}.7z";
                     filePath = fs.PathJoin(args.romset, zip);
                 }
 
                 // still not found: next
-                if (!fs.FileExists(filePath)) {
+                if (!fs.FileExists(filePath))
+                {
                     continue;
                 }
 
@@ -181,7 +208,8 @@ public class Roms : IRoms {
 
             messageHandler.Done($"Deleted {deleted} file(s)", args.selection);
         }
-        catch (Exception ex) {
+        catch (Exception ex)
+        {
             messageHandler.Error(ex);
         }
     }
@@ -193,10 +221,12 @@ public class Roms : IRoms {
     /// <param name="messageHandler">The message handler.</param>
     /// <exception cref="FileNotFoundException">Unable to find main CSV file</exception>
     /// <exception cref="DirectoryNotFoundException">Unable to find selection folder {args.selection}</exception>
-    public async Task Keep(Actions.RomsAction args, IMessageHandler messageHandler) {
+    public async Task Keep(Actions.RomsAction args, IMessageHandler messageHandler)
+    {
         messageHandler.Init("Filtering roms");
 
-        try {
+        try
+        {
             // check files and folders
             if (!fs.FileExists(args.main)) { throw new PathNotFoundException($"Unable to find main CSV file {args.main}"); }
             if (!fs.DirectoryExists(args.selection)) { throw new PathNotFoundException($"Unable to find selection folder {args.selection}"); }
@@ -213,7 +243,8 @@ public class Roms : IRoms {
             var deleted = 0;
 
             // check if files exist in games list
-            foreach (var f in files) {
+            foreach (var f in files)
+            {
                 if (messageHandler.MustCancel) { break; }
                 i++;
 
@@ -222,12 +253,14 @@ public class Roms : IRoms {
                 messageHandler.Progress(nameNoExt, total, i);
 
                 // don't auto-delete bios files
-                if (this.bioslist.Contains(nameNoExt, StringComparer.InvariantCultureIgnoreCase)) {
+                if (this.bioslist.Contains(nameNoExt, StringComparer.InvariantCultureIgnoreCase))
+                {
                     continue;
                 }
 
                 // delete if it's not found in the provided list
-                if (!content.Games.Any(c => c.Name.Equals(nameNoExt, StringComparison.InvariantCultureIgnoreCase))) {
+                if (!content.Games.Any(c => c.Name.Equals(nameNoExt, StringComparison.InvariantCultureIgnoreCase)))
+                {
                     fs.FileDelete(f);
                     deleted++;
                 }
@@ -235,18 +268,21 @@ public class Roms : IRoms {
 
             messageHandler.Done($"Deleted {deleted} files", args.selection);
         }
-        catch (Exception ex) {
+        catch (Exception ex)
+        {
             messageHandler.Error(ex);
         }
     }
 
-    private int CopyGamesList(Actions.RomsAction args, Models.CsvGamesList content, IMessageHandler messageHandler) {
+    private int CopyGamesList(Actions.RomsAction args, Models.CsvGamesList content, IMessageHandler messageHandler)
+    {
         var total = content.Games.Count;
         var i = 0;
         var copied = 0;
 
         // copy each file found in CSV
-        foreach (var game in content.Games.Select(g => g.Name)) {
+        foreach (var game in content.Games.Select(g => g.Name))
+        {
             if (messageHandler.MustCancel) { break; }
 
             i++;
@@ -260,14 +296,16 @@ public class Roms : IRoms {
             messageHandler.Progress(game, total, i);
 
             // check that source rom exists
-            if (!fs.FileExists(sourceRom)) {
+            if (!fs.FileExists(sourceRom))
+            {
                 zip = $"{game}.7z";
                 sourceRom = fs.PathJoin(args.romset, zip);
                 ext = "7z";
             }
 
             // still not found: next
-            if (!fs.FileExists(sourceRom)) {
+            if (!fs.FileExists(sourceRom))
+            {
                 continue;
             }
 
@@ -278,18 +316,22 @@ public class Roms : IRoms {
             messageHandler.Progress($"{game} ({fs.HumanSize(fileSize)})", total, i);
 
             // copy rom
-            if (!fs.FileExists(destRom) || args.overwrite) {
+            if (!fs.FileExists(destRom) || args.overwrite)
+            {
                 fs.FileCopy(sourceRom, destRom, true);
                 copied++;
             }
 
-            // try to copy bios if it's used and can be found
-            var biosesforgame = this.biosmatch.GetBiosesForGame(game);
-            foreach (var bios in biosesforgame) {
-                var sourcebios = fs.PathJoin(args.romset, $"{bios}.{ext}");
-                var destbios = fs.PathJoin(args.selection, $"{bios}.{ext}");
-                if (fs.FileExists(sourcebios) && !fs.FileExists(destbios)) {
-                    fs.FileCopy(sourcebios, destbios, args.overwrite);
+            // try to copy additional files for games if it's used and can be found
+            List<string> additionalFiles = [.. this.biosmatch.GetBiosesForGame(game), .. this.devicematch.GetDevicesForGame(game)];
+            foreach (var af in additionalFiles)
+            {
+                var sourceaf = fs.PathJoin(args.romset, $"{af}.{ext}");
+                var destaf = fs.PathJoin(args.selection, $"{af}.{ext}");
+                // never overwrite
+                if (fs.FileExists(sourceaf) && !fs.FileExists(destaf))
+                {
+                    fs.FileCopy(sourceaf, destaf, false);
                     copied++;
                 }
             }
@@ -297,7 +339,8 @@ public class Roms : IRoms {
             // try to copy chd if it can be found
             var sourceChd = fs.PathJoin(args.romset, game);
             var targetChd = fs.PathJoin(args.selection, game);
-            if (fs.DirectoryExists(sourceChd)) {
+            if (fs.DirectoryExists(sourceChd))
+            {
                 if (messageHandler.MustCancel) { break; }
 
                 messageHandler.Progress($"Copying {game} CHD ({fs.HumanSize(fs.DirectorySize(sourceChd))})", total, i);
@@ -312,54 +355,77 @@ public class Roms : IRoms {
     /// <summary>
     /// A game/bios match
     /// </summary>
-    private sealed class BiosMatch {
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BiosMatch"/> class.
-        /// </summary>
-        /// <param name="game">The game.</param>
-        /// <param name="bios">The bios.</param>
-        /// <param name="clone">The clone.</param>
-        public BiosMatch(string game, string bios, string clone) {
-            this.Game = game;
-            this.Bios = bios;
-            this.Clone = clone;
-        }
-
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="BiosMatch"/> class.
+    /// </remarks>
+    /// <param name="game">The game.</param>
+    /// <param name="bios">The bios.</param>
+    /// <param name="clone">The clone.</param>
+    private sealed class BiosMatch(string game, string bios, string clone)
+    {
         /// <summary>
         /// Gets or sets the bios name.
         /// </summary>
-        public string Bios { get; set; }
+        public string Bios { get; set; } = bios;
 
         /// <summary>
         /// Gets or sets the clone.
         /// </summary>
-        public string Clone { get; set; }
+        public string Clone { get; set; } = clone;
 
         /// <summary>
         /// Gets or sets the game name.
         /// </summary>
-        public string Game { get; set; }
+        public string Game { get; set; } = game;
     }
 
     /// <summary>
     /// A game/bios matches list
     /// </summary>
-    /// <seealso cref="System.Collections.Generic.List&lt;ArcadeManager.Services.Roms.BiosMatch&gt;"/>
-    private sealed class BiosMatchList : List<BiosMatch> {
-
+    private sealed class BiosMatchList : List<BiosMatch>
+    {
         /// <summary>
         /// Gets the bioses for a game (it might change between MAME/FBNeo versions).
         /// </summary>
         /// <param name="game">The game name.</param>
         /// <returns>The potential bioses names</returns>
-        public IEnumerable<string> GetBiosesForGame(string game) {
+        public IEnumerable<string> GetBiosesForGame(string game)
+        {
             var result = new List<string>();
-            foreach (var item in this.Where(bm => bm.Game.Equals(game, StringComparison.InvariantCultureIgnoreCase))) {
+            foreach (var item in this.Where(bm => bm.Game.Equals(game, StringComparison.InvariantCultureIgnoreCase)))
+            {
                 result.Add(item.Bios);
             }
 
             return result;
+        }
+    }
+
+    /// <summary>
+    /// A game/devices match
+    /// </summary>
+    /// <param name="game">The game name</param>
+    /// <param name="devices">The devices names</param>
+    private sealed class DeviceMatch(string game, string[] devices)
+    {
+        public string Game { get; set; } = game;
+        public string[] Devices { get; set; } = devices;
+    }
+
+    /// <summary>
+    /// A game/device matches list
+    /// </summary>
+    private sealed class DeviceMatchList : List<DeviceMatch>
+    {
+        public IEnumerable<string> GetDevicesForGame(string game)
+        {
+            var match = this.FirstOrDefault(dm => dm.Game.Equals(game, StringComparison.InvariantCultureIgnoreCase));
+            if (match != null)
+            {
+                return match.Devices;
+            }
+
+            return [];
         }
     }
 }
